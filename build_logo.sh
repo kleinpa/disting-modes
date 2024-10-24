66b96f320ecddda1099288d67f322abbba11fffd
Peter Klein <kleinpa00@gmail.com>
2024-10-24 05:50:41 +0000

    Use pnpm

--- build_logo.sh
+++ build_logo.sh
@@ -1,12 +1,22 @@
-#/bin/bash -ue
+#!/bin/bash -ue
 
 in=logo.svg
 
 tmp=$(mktemp -d)
 trap "rm -rf $tmp" EXIT
 flags="-background none"
-convert $flags -resize x512 $in public/logo512.png
-convert $flags -resize x192 $in public/logo192.png
-convert $flags -resize x16 -gravity center -crop 16x16+0+0 -flatten -colors 256 $in $tmp/favicon-16x16.ico
-convert $flags -resize x32 -gravity center -crop 32x32+0+0 -flatten -colors 256 $in $tmp/favicon-32x32.ico
-convert $flags $tmp/favicon-16x16.ico $tmp/favicon-32x32.ico public/favicon.ico
+
+if [ ! -f public/logo512.png ]; then
+    convert $flags -resize x512 $in public/logo512.png
+fi
+
+if [ ! -f public/logo192.png ]; then
+    convert $flags -resize x192 $in public/logo192.png
+fi
+
+if [ ! -f public/favicon.ico ]; then
+    convert $flags -resize x16 -gravity center -crop 16x16+0+0 -flatten -colors 256 $in $tmp/favicon-16x16.ico
+    convert $flags -resize x32 -gravity center -crop 32x32+0+0 -flatten -colors 256 $in $tmp/favicon-32x32.ico
+
+    convert $flags $tmp/favicon-16x16.ico $tmp/favicon-32x32.ico public/favicon.ico
+fi
